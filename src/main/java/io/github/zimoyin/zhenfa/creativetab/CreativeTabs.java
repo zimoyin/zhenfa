f08a04cabc9b1ed068f073f6b8721a0a53188f0f
zimoyin <2556608754@qq.com>
2025-03-21 00:17:02 +0800

    开发准备

--- src/main/java/io/github/zimoyin/zhenfa/creativetab/CreativeTabs.java
+++ src/main/java/io/github/zimoyin/zhenfa/creativetab/CreativeTabs.java
@@ -8,8 +8,8 @@ import net.minecraft.world.item.CreativeModeTab;
  * 用于声明测试标签
  */
 public class CreativeTabs {
-    public static final CreativeModeTab TestCreativeTab = CreativeModeTabBuilder.builder("test")
-            .lang(Lang.of(Lang.LangType.ZH_CN, "测试标签"))
+    public static final CreativeModeTab TestCreativeTab = CreativeModeTabBuilder.builder("zhenfa")
+            .lang(Lang.of(Lang.LangType.ZH_CN, "阵法"))
             .build();
     public static final CreativeModeTab TestCreativeTab2 = CreativeModeTabBuilder.create("test2");
 }
--- src/main/java/io/github/zimoyin/zhenfa/datagen/README.md
+++
@@ -1,3 +0,0 @@
-
-
-datagen 包是用于自动生成 json 的。当你运行 gradle 的 task `runData` 时，它将自动生成 json 文件。
